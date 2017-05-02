9f6ba15d5b672b3bc779a20a0f79a4019292e2b3
Przemysław <przemyslaw.ciacka@gmail.com>
2017-05-02 14:18:53 +0200

    Get products from database

--- src/main/java/com/codecool/shop/dao/ProductDaoSqlite.java
+++ src/main/java/com/codecool/shop/dao/ProductDaoSqlite.java
@@ -4,9 +4,7 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,22 +29,30 @@ public class ProductDaoSqlite implements ProductDao {
 
     @Override
     public List<Product> getAll() {
+        List<Product> products = new ArrayList<Product>();
+        ProductCategory category = new ProductCategory("Category", "Department", "Description");
+        Supplier supplier = new Supplier("Supplier", "Description");
+
         try {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from products");
+            while(rs.next()) {
+                Product product = new Product(
+                        rs.getString("name"),
+                        rs.getFloat("price"),
+                        "PLN",
+                        rs.getString("description"),
+                        category,
+                        supplier
+                );
+                products.add(product);
+            }
         } catch(SQLException e) {
             System.out.println("Connect to DB failed");
             System.out.println(e.getMessage());
         }
 
-        List<Product> products = new ArrayList<Product>();
-        ProductCategory category = new ProductCategory("Category", "Department", "Description");
-        Supplier supplier = new Supplier("Supplier", "Description");
-        Product product1 = new Product("Product 1", 12.50f, "PLN", "Opis", category, supplier);
-        Product product2 = new Product("Product 2", 12.50f, "PLN", "Opis", category, supplier);
-        Product product3 = new Product("Product 3", 12.50f, "PLN", "Opis", category, supplier);
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
         return products;
     }
 
