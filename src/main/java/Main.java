7fe079d10adc465a7c7569d04f0b9b714d8cce2b
Przemysław <przemyslaw.ciacka@gmail.com>
2017-05-02 14:34:54 +0200

    Create tables only of --create-table arg is passed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,19 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
-            Statement statement = connection.createStatement();
-            statement.executeQuery("CREATE TABLE IF NOT EXISTS products\n" +
-                    "(\n" +
-                    "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
-                    "    name VARCHAR NOT NULL,\n" +
-                    "    description TEXT,\n" +
-                    "    price DOUBLE DEFAULT 0.00 NOT NULL\n" +
-                    ")");
-        } catch (SQLException e) {
+        if(args.length > 0 && args[0].equals("--create-tables")) {
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+                Statement statement = connection.createStatement();
+                statement.executeQuery("CREATE TABLE IF NOT EXISTS products\n" +
+                        "(\n" +
+                        "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                        "    name VARCHAR NOT NULL,\n" +
+                        "    description TEXT,\n" +
+                        "    price DOUBLE DEFAULT 0.00 NOT NULL\n" +
+                        ")");
+            } catch (SQLException e) {
+            }
         }
 
         ProductController productController = new ProductController();
