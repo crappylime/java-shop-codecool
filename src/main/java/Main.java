8116c63c2ba8f560cfa50687cf359d3b40dc7bac
Przemysław <przemyslaw.ciacka@gmail.com>
2017-05-02 11:39:39 +0200

    Display list of products

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,26 @@
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Product> products = new ArrayList<Product>();
+        ProductCategory category = new ProductCategory("Category", "Department", "Description");
+        Supplier supplier = new Supplier("Supplier", "Description");
+        Product product1 = new Product("Product 1", 12.50f, "PLN", "Opis", category, supplier);
+        Product product2 = new Product("Product 2", 12.50f, "PLN", "Opis", category, supplier);
+        Product product3 = new Product("Product 3", 12.50f, "PLN", "Opis", category, supplier);
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
 
+        for(Product p: products) {
+            System.out.println(p.getName());
+        }
     }
 
 
