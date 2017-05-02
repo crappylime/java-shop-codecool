a71f360e5a45e14deb32d40ef691333a50124d4d
Przemysław <przemyslaw.ciacka@gmail.com>
2017-05-02 13:17:27 +0200

    Implement view in order to display products list

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.view.ProductView;
 
 import java.util.List;
 
@@ -11,10 +12,8 @@ public class ProductController {
     public void listProducts() {
         ProductDao productDao = new ProductDaoSqlite();
         List<Product> products = productDao.getAll();
-
-        for(Product p: products) {
-            System.out.println(p.getName());
-        }
+        ProductView view = new ProductView();
+        view.displayProductsList(products);
     }
 
 }
--- src/main/java/com/codecool/shop/view/ProductView.java
+++ src/main/java/com/codecool/shop/view/ProductView.java
@@ -1,4 +1,15 @@
 package com.codecool.shop.view;
 
+import com.codecool.shop.model.Product;
+
+import java.util.List;
+
 public class ProductView {
+
+    public void displayProductsList(List<Product> products) {
+        for(Product p: products) {
+            System.out.println(p.getName());
+        }
+    }
+
 }
