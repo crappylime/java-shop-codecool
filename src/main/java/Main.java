940328d69b1fe67ade3650cd9dee38289e914c8b
Przemysław <przemyslaw.ciacka@gmail.com>
2017-05-02 14:28:55 +0200

    Create products table when app start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
 import com.codecool.shop.controller.ProductController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+            Statement statement = connection.createStatement();
+            statement.executeQuery("CREATE TABLE IF NOT EXISTS products\n" +
+                    "(\n" +
+                    "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "    name VARCHAR NOT NULL,\n" +
+                    "    description TEXT,\n" +
+                    "    price DOUBLE DEFAULT 0.00 NOT NULL\n" +
+                    ")");
+        } catch (SQLException e) {
+        }
+
         ProductController productController = new ProductController();
         productController.listProducts();
     }
