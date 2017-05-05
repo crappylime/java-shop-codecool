745c80fafd268c0c6b5826ecaff9a76a2bfa2692
Przemysław <przemyslaw.ciacka@gmail.com>
2017-05-05 09:57:54 +0200

    Add product to basket

--- src/main/java/com/codecool/shop/model/Basket.java
+++ src/main/java/com/codecool/shop/model/Basket.java
@@ -5,4 +5,27 @@ import java.util.List;
 
 public class Basket {
     private List<BasketItem> items = new ArrayList<>();
+
+    public void add(Product product, Integer quantity) {
+        boolean productExists = false;
+        for(BasketItem item: this.getItems()) {
+            if(item.getProduct().equals(product)) {
+                item.setQuantity(item.getQuantity() + quantity);
+                productExists = true;
+            }
+        }
+
+        if(!productExists) {
+            BasketItem item = new BasketItem(product, quantity);
+            this.getItems().add(item);
+        }
+    }
+
+    public List<BasketItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<BasketItem> items) {
+        this.items = items;
+    }
 }
