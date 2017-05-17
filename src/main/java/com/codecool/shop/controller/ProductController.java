f4d63c2da0db8d54e75f8380b561ac09c8f5c8b9
Marta Sajdak <crappylime@gmail.com>
2017-05-17 15:35:51 +0200

    Implement showing products by supplier

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,11 +1,9 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductCategoryDaoSqlite;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.ProductDaoSqlite;
+import com.codecool.shop.dao.*;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -17,17 +15,24 @@ import java.util.Map;
 public class ProductController extends BaseController {
     private ProductDao productDao = new ProductDaoSqlite();
     private ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite();
+    private SupplierDao supplierDao = new SupplierDaoSqlite();
 
     public String showProducts(Request req, Response res) {
         Map params = new HashMap<>();
         List<Product> products = productDao.getAll();
         List<ProductCategory> categories = productCategoryDao.getAll();
+        List<Supplier> suppliers = supplierDao.getAll();
 
         if (!(req.queryParams("category_id") == null)) {
             ProductCategory category = productCategoryDao.find(Integer.parseInt(req.queryParams("category_id")));
             products = productDao.getBy(category);
         }
+        if (!(req.queryParams("supplier_id") == null)) {
+            Supplier supplier = supplierDao.find(Integer.parseInt(req.queryParams("supplier_id")));
+            products = productDao.getBy(supplier);
+        }
         params.put("categories", categories);
+        params.put("suppliers", suppliers);
         params.put("products", products);
 
         ModelAndView modelAndView = new ModelAndView(params, "product/index");
