55d1bf003dcfcf5b2585f5341cb5c8681dcc5e2b
Michał Kaczkowski <michal2302@gmail.com>
2017-05-17 16:06:27 +0200

    Check search query length

---
+++ src/main/resources/public/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by michal on 12/05/2017.
+ */
+
+
+function checkLength(mes){
+
+    var textbox = document.getElementById("searchbox").value.length;
+    if (textbox >= 3) {
+            document.location.href = mes.href
+    }
+    document.getElementById("length_error").style.display = "inline";
+}
