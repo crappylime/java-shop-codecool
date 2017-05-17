d425122bdd541527a57621178276c67139c66a76
Marta Sajdak <crappylime@gmail.com>
2017-05-17 15:17:09 +0200

    Implement showing products by category

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -6,44 +6,35 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.model.Supplier;
 import com.codecool.shop.view.ProductView;
-import com.codecool.shop.view.UserInput;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
-import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class ProductController extends BaseController{
+public class ProductController extends BaseController {
     private ProductDao productDao = new ProductDaoSqlite();
     private ProductCategoryDao productCategoryDao = new ProductCategoryDaoSqlite();
     private ProductView view = new ProductView();
 
-
-
     public String showProducts(Request req, Response res) {
         Map params = new HashMap<>();
-
         List<Product> products = productDao.getAll();
         List<ProductCategory> categories = productCategoryDao.getAll();
-        params.put("products", products);
+
+        if (!(req.queryParams("category_id") == null)) {
+            ProductCategory category = productCategoryDao.find(Integer.parseInt(req.queryParams("category_id")));
+            products = productDao.getBy(category);
+        }
         params.put("categories", categories);
+        params.put("products", products);
 
         ModelAndView modelAndView = new ModelAndView(params, "product/index");
 
         return render(modelAndView);
-    }
-
-    public void listProductsByCategory() {
-        List<ProductCategory> categories = productCategoryDao.getAll();
-        view.displayCategoriesList(categories);
-
-        Integer categoryId = UserInput.getUserInput();
-        ProductCategory category = productCategoryDao.find(categoryId);
-        List<Product> products = productDao.getBy(category);
-        view.displayProductsList(products);
     }
 
 }
