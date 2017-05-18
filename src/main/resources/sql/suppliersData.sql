9e1a1b8d52201acddd6e90ad07d042e6372a397e
Marta Sajdak <crappylime@gmail.com>
2017-05-18 14:16:58 +0200

    Prepare fake data

--- src/main/resources/sql/suppliersData.sql
+++ src/main/resources/sql/suppliersData.sql
@@ -1,1 +1,8 @@
-INSERT INTO suppliers (name, description) VALUES ('Sup', 'SupDESC')
+INSERT INTO suppliers (name, description) VALUES ('Ariel', 'Najczerwieńsza jakość');
+INSERT INTO suppliers (name, description) VALUES ('Ludwik', 'Produkt niskiej jakości lecz tani');
+INSERT INTO suppliers (name, description) VALUES ('Mlekovita', 'Zwierzęta mamy gdzieś');
+INSERT INTO suppliers (name, description) VALUES ('Good Lood', 'Lody bez chemii');
+INSERT INTO suppliers (name, description) VALUES ('Żółte kurczaki', 'Kurczaki, którym skracamy cierpienie');
+INSERT INTO suppliers (name, description) VALUES ('Hammers', 'Narzędzia jak narzędzia, nie zepsują się po pierwszym użyciu');
+INSERT INTO suppliers (name, description) VALUES ('Samsung', 'Twój czas należy do nas');
+INSERT INTO suppliers (name, description) VALUES ('Bejtons', 'Batony dzięki którym wysiedzisz dłużej przed komputerem');
