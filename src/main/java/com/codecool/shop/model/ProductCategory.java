6d266618267a0464433df5473e8732be30834c7f
sebmichalowski <seb.michalowski@gmail.com>
2017-05-31 16:57:07 +0200

    Product - delete Product usage from ProductCategory

--- src/main/java/com/codecool/shop/model/ProductCategory.java
+++ src/main/java/com/codecool/shop/model/ProductCategory.java
@@ -1,21 +1,16 @@
 package com.codecool.shop.model;
 
-import java.util.ArrayList;
-
 public class ProductCategory extends BaseModel {
     private String department;
-    private ArrayList<Product> products;
 
     public ProductCategory(String name, String description, String department) {
         super(name, description);
         this.department = department;
-        this.products = new ArrayList<>();
     }
 
     public ProductCategory(int id, String name, String description, String department) {
         super(id, name, description);
         this.department = department;
-        this.products = new ArrayList<>();
     }
 
     public String getDepartment() {
@@ -24,18 +19,6 @@ public class ProductCategory extends BaseModel {
 
     public void setDepartment(String department) {
         this.department = department;
-    }
-
-    public void setProducts(ArrayList<Product> products) {
-        this.products = products;
-    }
-
-    public ArrayList getProducts() {
-        return this.products;
-    }
-
-    public void addProduct(Product product) {
-        this.products.add(product);
     }
 
     public String toString() {
