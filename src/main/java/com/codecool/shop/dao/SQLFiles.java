3f42e99d40985658c582321291aed3fc1b452987
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-01 08:55:57 +0200

    Delete path to database from SQLFiles enum

--- src/main/java/com/codecool/shop/dao/SQLFiles.java
+++ src/main/java/com/codecool/shop/dao/SQLFiles.java
@@ -1,7 +1,6 @@
 package com.codecool.shop.dao;
 
 public enum SQLFiles {
-    DATABASE_FILE("jdbc:sqlite:src/main/resources/database.db"),
     PRODUCTS_DATA("src/main/resources/sql/productsData.sql"),
     PRODUCTS("src/main/resources/sql/products.sql"),
     CATEGORIES_DATA("src/main/resources/sql/categoriesData.sql"),
